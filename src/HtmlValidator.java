e74c970131010fff67d27492f8711b8488ff0faf
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2023-09-27 13:09:56 -0500

    test 100%, se sube img de la comprobacion

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -6,29 +6,43 @@ public class HtmlValidator {
     public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
         /* IMPLEMENT THIS METHOD! */
         Stack<HtmlTag> tagStack = new Stack<>();
+
+        // Itera a través de las etiquetas en la cola hasta que esté vacía
         while (!tags.isEmpty()) {
             HtmlTag tag = tags.poll();
+
+            // Verifica si la etiqueta no es auto-cierre
             if (!tag.isSelfClosing()) {
                 if (tag.isOpenTag()) {
+                    // Es una etiqueta de apertura, la agregamos a la pila
                     tagStack.push(tag);
                 } else {
                     // Es una etiqueta de cierre
                     if (tagStack.isEmpty()) {
+                        // Si la pila está vacía y encontramos una etiqueta de cierre, el HTML no es válido
                         return null;
                     }
+                    // Compara la etiqueta de cierre con la etiqueta en la cima de la pila
                     if (tagStack.peek().matches(tag)) {
+                        // Si coinciden, la etiqueta de apertura correspondiente se retira de la pila
                         tagStack.pop();
                     } else {
+                        // Si no coinciden, el HTML no es válido
                         return tagStack;
                     }
                 }
             }
         }
+
         // Verifica si quedan etiquetas de apertura sin cerrar
         if (!tagStack.isEmpty()) {
+            // Si la pila no está vacía al final, significa que hay etiquetas de apertura sin cerrar
             return tagStack;
         }
 
+        // Si no se encontraron problemas de emparejamiento de etiquetas, se devuelve una pila vacía
         return new Stack<>();
     }
+
 }
+
