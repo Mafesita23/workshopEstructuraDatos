c1616f11db67a1caa11a6f295bd87216df981f95
mariapantoja <mariaf.pantojac@unilibrebog.edu.co>
2023-09-27 12:04:13 -0500

    prueba 100%

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -1,16 +1,36 @@
 import java.util.Queue;
 import java.util.Stack;
 
-
 public class HtmlValidator {
+	// Función para validar etiquetas HTML
 
 	public static Stack<HtmlTag> isValidHtml(Queue<HtmlTag> tags) {
+		Stack<HtmlTag> stack = new Stack<>();
 
-		/* IMPLEMENT THIS METHOD! */
+		for (HtmlTag tag : tags) {
+			// Si es una etiqueta auto-cerrada, se ignora
+			if (tag.isSelfClosing()) {
+				// Ignorar etiquetas auto-cerradas
+				continue;
+			} else if (tag.isOpenTag()) {
+				// Si es una etiqueta de apertura, se agrega a la pila
+				stack.push(tag);
+			} else if (!tag.isOpenTag()) {
 
-		return null; // this line is here only so this code will compile if you don't modify it
+				// Es una etiqueta de cierre
+				if (!stack.isEmpty()) {
+					// Verifica si la etiqueta de cierre coincide con la etiqueta superior en la pila
+					if (stack.peek().matches(tag)) {
+						stack.pop(); //  La etiqueta coincide, la eliminamos de la pila
+					} else {
+						return stack; // La etiqueta de cierre no coincide, devolvemos la pila actual
+					}
+				} else {
+					return null; // La etiqueta de cierre sin etiqueta de apertura correspondiente, devuelve null
+				}
+			}
+		}
+
+		return stack.isEmpty() ? new Stack<>() : stack; // Al final del proceso, verifica si hay etiquetas de apertura no cerradas
 	}
-	
-
 }
-
